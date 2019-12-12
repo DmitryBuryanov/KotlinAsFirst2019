8401ce058fffccb914420ae942ac276ce2661f93
DmitryBuryanov <buryanov.dv@edu.spbstu.ru>
2019-12-13 01:04:42 +0300

    8

--- src/lesson8/task1/Geometry.kt
+++ src/lesson8/task1/Geometry.kt
@@ -179,7 +179,7 @@ class Line private constructor(val b: Double, val angle: Double) {
  * Построить прямую по отрезку
  */
 fun lineBySegment(s: Segment): Line {
-    val angle = atan(abs((s.begin.y - s.end.y) / (s.begin.x - s.end.x)))
+    val angle = if (s.end.x == s.begin.x) PI / 2 else atan((s.end.y - s.begin.y) / (s.end.x - s.begin.x))
     return Line(Point(s.begin.x, s.begin.y), angle)
 }
 
@@ -189,7 +189,7 @@ fun lineBySegment(s: Segment): Line {
  * Построить прямую по двум точкам
  */
 fun lineByPoints(a: Point, b: Point): Line {
-    val angle = atan((b.y - a.y) / (b.x - a.x))
+    val angle = if (b.x == a.x) PI / 2 else atan((b.y - a.y) / (b.x - a.x))
     return Line(a, angle)
 }
 
@@ -199,9 +199,9 @@ fun lineByPoints(a: Point, b: Point): Line {
  * Построить серединный перпендикуляр по отрезку или по двум точкам
  */
 fun bisectorByPoints(a: Point, b: Point): Line {
-    val x1 = abs(b.x + a.x) / 2
-    val y1 = abs(b.y + a.y) / 2
-    val angle = PI / 2 - atan((b.y - a.y) / (b.x - a.x))
+    val x1 = (b.x + a.x) / 2
+    val y1 = (b.y + a.y) / 2
+    val angle = (PI / 2 - atan((b.y - a.y) / (b.x - a.x))) % PI
     return Line(Point(x1, y1), angle)
 }
 
